d67e67d21c486850c336ff31764bb37d83594f7e
Omar Khaled Essam <omar.noureldin800@gmail.com>
2024-10-05 17:12:02 +0300

    Added the profile name and the 2 white containers

--- buisness_card_app/lib/main.dart
+++ buisness_card_app/lib/main.dart
@@ -23,7 +23,86 @@ class BuisnessCardApp extends StatelessWidget {
                 radius: 120,
                 backgroundImage: AssetImage('images/tharwat.png'),
               ),
-            )
+            ),
+            Text(
+              'Omar Khaled',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontFamily: 'Pacifico',
+              ),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                  color: Color(0xFF6C8090),
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                ),
+                height: 65,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 24),
+                      child: Icon(
+                        Icons.phone,
+                        size: 32,
+                        color: Color(0xFF2B475E),
+                      ),
+                    ),
+                    Spacer(
+                      flex: 1,
+                    ),
+                    Text(
+                      '+20 0117635342',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    Spacer(
+                      flex: 2,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                ),
+                height: 65,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 24),
+                      child: Icon(
+                        Icons.mail,
+                        size: 32,
+                        color: Color(0xFF2B475E),
+                      ),
+                    ),
+                    Spacer(
+                      flex: 1,
+                    ),
+                    Text(
+                      'info@email.com',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    Spacer(
+                      flex: 2,
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
